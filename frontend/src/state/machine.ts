78de6024e3e252667371671fea8930ea00ce9c97
pociej <grzegorz.pociejewski@gmail.com>
2023-08-22 12:40:11 +0200

    fix: husky flow

--- frontend/src/state/machine.ts
+++ frontend/src/state/machine.ts
@@ -8,7 +8,7 @@ export const onboardingMachine = createMachine<
   { type: Commands.NEXT } | { type: Commands.PREVIOUS }
 >({
   id: 'onboarding',
-  initial: Steps.SHOW_METAMASK_LINK,
+  initial: Steps.WELCOME,
   states: {
     [Steps.CHOOSE_NETWORK]: {},
     [Steps.ON_RAMP]: {},
