8beaa2766c504d685485333d54e268f97e2d506a
pociej <grzegorz.pociejewski@gmail.com>
2023-09-22 10:40:00 +0200

    fix: reove loading spinner from button

--- frontend/src/components/atoms/button/button.tsx
+++ frontend/src/components/atoms/button/button.tsx
@@ -1,13 +1,12 @@
 import { forwardRef, ComponentProps } from 'react'
 
 import styles from './button.module.css'
-import { LoadingSpinner } from '../loadingSpinner'
 
 const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
   ({ children, className = styles.button, ...rest }, ref) => {
     return (
       <button ref={ref} className={className} {...rest}>
-        <LoadingSpinner />
+        {children}
       </button>
     )
   }
