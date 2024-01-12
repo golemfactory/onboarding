fb1170ff906d49036c914c34c7e536d831a5c48a
pociej <grzegorz.pociejewski@gmail.com>
2024-01-12 09:07:28 +0100

    fix: include unsupported in i18 config

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -2,6 +2,7 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import landingEn from './en/landing.json'
 import landingEs from './es/landing.json'
+import unsupportedEn from './en/unsupported.json'
 
 i18next.use(initReactI18next).init({
   lng: 'en',
@@ -9,6 +10,7 @@ i18next.use(initReactI18next).init({
   resources: {
     en: {
       landing: landingEn,
+      unsupported: unsupportedEn,
     },
     es: {
       landing: landingEs,
