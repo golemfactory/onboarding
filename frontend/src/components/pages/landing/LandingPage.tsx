70d113388af08b314a014505a5dcb51ce3044a90
pociej <grzegorz.pociejewski@gmail.com>
2024-02-09 16:38:11 +0100

    chore: bring back onboarding

--- frontend/src/components/pages/landing/LandingPage.tsx
+++ frontend/src/components/pages/landing/LandingPage.tsx
@@ -104,42 +104,18 @@ export const LandingPage = () => {
   }
   return (
     <LayoutTemplate
-    // header={
-    //   // <Button
-    //   //   buttonStyle="solid"
-    //   //   className="md:py-4 md:px-9 py-2 px-5 text-button-large mt-8"
-    //   //   useDefault={true}
-    //   //   onClick={startOnboarding}
-    //   // >
-    //   //   {/* <Trans i18nKey="getGLM" ns="landing" /> */}
-    //   // </Button>
-    // }
+      header={
+        <Button
+          buttonStyle="solid"
+          className="md:py-4 md:px-9 py-2 px-5 text-button-large mt-8"
+          useDefault={true}
+          onClick={startOnboarding}
+        >
+          <Trans i18nKey="getGLM" ns="landing" />
+        </Button>
+      }
     >
-      {/* <LandingPageContent startOnboarding={startOnboarding} /> */}
-      <div className="col-span-10 col-start-2 text-h3 text-primary mt-20 text-center grid grid-cols-4">
-        <div className="col-span-2 col-start-2">
-          <img
-            src="/placeholder.svg"
-            className="mb-20 w-[50vw] text-center margin-auto"
-          ></img>
-        </div>
-        <div className="col-span-4">
-          This site is temporarily unavailable. We apologize for any
-          inconvenience and appreciate your patience.
-          <br></br>
-          It will be back up soon! In the meantime, you can find the previous
-          website with information about GLM token here:
-          <br></br>
-          <a
-            href="https://migration-tracker.golem.network/"
-            rel="noreferrer"
-            target="_blank"
-            className="text-primary underline inline"
-          >
-            https://migration-tracker.golem.network
-          </a>
-        </div>
-      </div>
+      <LandingPageContent startOnboarding={startOnboarding} />
     </LayoutTemplate>
   )
 }
