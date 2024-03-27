6b8da90fd82bc281fe5fdb48308dece00974227d
pociej <grzegorz.pociejewski@gmail.com>
2024-03-27 10:31:02 +0100

    chore: use light theme of walleConnect

--- frontend/src/components/providers/Blockchain.provider.tsx
+++ frontend/src/components/providers/Blockchain.provider.tsx
@@ -31,6 +31,7 @@ createWeb3Modal({
     '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
   ],
+  themeMode: 'light',
 })
 
 export function BlockchainProvider({ children }: PropsWithChildren) {
